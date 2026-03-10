69c34ba57de2431b3ad6c08ab2df9a979968f503
Kavita <bathyalkavita3@gmail.com>
2026-03-10 11:59:40 +0530

    commit practical mst

---
+++ fetch-users-app/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div>
+      <h1>Fetch Users from API</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ fetch-users-app/vite-project/src/components/Users.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <b>{user.name}</b> - {user.email}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default Users;
